f58e0d6915c02af9b39c9ab00a13710ecfadc561
stuartdd <stuart.d.davies@btinternet.com>
2019-10-18 23:06:22 +0100

    Test

--- servermain/serverInstanceData.go
+++ servermain/serverInstanceData.go
@@ -112,6 +112,7 @@ ListenAndServeOnPort start the server on a specific port
 func (p *ServerInstanceData) ListenAndServeOnPort(port int) {
 	p.server = &http.Server{Addr: ":" + strconv.Itoa(port)}
 	p.server.Handler = p
+	fmt.Printf("Server -----------------: %n", port)
 	err := p.server.ListenAndServe()
 	if p.GetServerClosedReason() != "" {
 		p.logger.LogInfof("Server Halted: %s", p.GetServerClosedReason())
